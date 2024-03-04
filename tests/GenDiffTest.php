058910ab2b52ab43c1f1859ccc1d3425498beeec
Severonik <severov_1996@mail.ru>
2024-03-04 15:06:23 +0300

    new test 4

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -12,14 +12,16 @@ class GenDiffTest extends TestCase
     {
         $pathToFile1 = __DIR__ . '/fixtures/file1.json';
         $pathToFile2 = __DIR__ . '/fixtures/file2.json';
-        $expectedResult = '{
+        $expectedResult = <<<JSON
+        {
           - follow: false
             host: "hexlet.io"
           - proxy: "123.234.53.22"
           - timeout: 50
           + timeout: 20
           + verbose: true
-        }';
+        }
+        JSON;
         
         $this->assertEquals($expectedResult, genDiff($pathToFile1, $pathToFile2));
     }
