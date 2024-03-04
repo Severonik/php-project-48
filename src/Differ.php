a4526e2deb1beb56b96f703a483d0d0ee41d3e26
Severonik <severov_1996@mail.ru>
2024-03-04 15:50:53 +0300

    genDiff

--- src/Differ.php
+++ src/Differ.php
@@ -6,17 +6,9 @@ use function Hexlet\Code\Parsers\parse;
 
 function genDiff($pathToFile1, $pathToFile2)
 {
-    // Получаем форматы файлов
-    $format1 = pathinfo($pathToFile1, PATHINFO_EXTENSION);
-    $format2 = pathinfo($pathToFile2, PATHINFO_EXTENSION);
-
-    // Получаем содержимое файлов
-    $content1 = file_get_contents($pathToFile1);
-    $content2 = file_get_contents($pathToFile2);
-
-    // Парсим содержимое файлов
-    $data1 = parse($content1, $format1);
-    $data2 = parse($content2, $format2);
+    // Получаем данные из файлов
+    $data1 = getDataFromFile($pathToFile1);
+    $data2 = getDataFromFile($pathToFile2);
 
     // Сравниваем данные и строим результат сравнения
     $diff = buildDiff($data1, $data2);
@@ -25,26 +17,50 @@ function genDiff($pathToFile1, $pathToFile2)
     return formatDiff($diff);
 }
 
+function getDataFromFile($pathToFile)
+{
+    // Получаем формат файла
+    $format = pathinfo($pathToFile, PATHINFO_EXTENSION);
+
+    // Получаем содержимое файла и парсим его
+    $content = file_get_contents($pathToFile);
+    return parse($content, $format);
+}
+
 function buildDiff($data1, $data2)
 {
-    $keys = array_unique(array_merge(array_keys($data1), array_keys($data2)));
-    sort($keys);
+    $keys = getUniqueKeys($data1, $data2);
 
     $diff = [];
     foreach ($keys as $key) {
-        if (!array_key_exists($key, $data2)) {
-            $diff[] = "  - {$key}: " . json_encode($data1[$key]);
-        } elseif (!array_key_exists($key, $data1)) {
-            $diff[] = "  + {$key}: " . json_encode($data2[$key]);
-        } elseif ($data1[$key] === $data2[$key]) {
-            $diff[] = "    {$key}: " . json_encode($data1[$key]);
-        } else {
-            $diff[] = "  - {$key}: " . json_encode($data1[$key]);
-            $diff[] = "  + {$key}: " . json_encode($data2[$key]);
-        }
+        $diff[] = buildDiffItem($key, $data1, $data2);
     }
 
     return $diff;
+}
+
+function getUniqueKeys($data1, $data2)
+{
+    $keys = array_unique(array_merge(array_keys($data1), array_keys($data2)));
+    sort($keys);
+    return $keys;
+}
+
+function buildDiffItem($key, $data1, $data2)
+{
+    if (!array_key_exists($key, $data2)) {
+        return "  - {$key}: " . json_encode($data1[$key]);
+    }
+    if (!array_key_exists($key, $data1)) {
+        return "  + {$key}: " . json_encode($data2[$key]);
+    }
+    if ($data1[$key] === $data2[$key]) {
+        return "    {$key}: " . json_encode($data1[$key]);
+    }
+    return [
+        "  - {$key}: " . json_encode($data1[$key]),
+        "  + {$key}: " . json_encode($data2[$key]),
+    ];
 }
 
 function formatDiff($diff)
