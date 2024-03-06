c7db7332d3d00f5bac6ffaa230707c892c373198
Severonik <severov_1996@mail.ru>
2024-03-06 16:35:26 +0300

    new plain 3

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -54,20 +54,18 @@ class GenDiffTest extends TestCase
     {
         $pathToFile1 = __DIR__ . '/fixtures/filepath1.yaml';
         $pathToFile2 = __DIR__ . '/fixtures/filepath2.yaml';
-        $expectedResult = <<<PLAIN
-        Property 'common.follow' was added with value: false
-        Property 'common.setting2' was removed
-        Property 'common.setting3' was updated. From true to null
-        Property 'common.setting4' was added with value: 'blah blah'
-        Property 'common.setting5' was added with value: [complex value]
-        Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-        Property 'common.setting6.ops' was added with value: 'vops'
-        Property 'group1.baz' was updated. From 'bas' to 'bars'
-        Property 'group1.nest' was updated. From [complex value] to 'str'
-        Property 'group2' was removed
-        Property 'group3' was added with value: [complex value]
-        PLAIN;
-
-        $this->assertEquals(trim($expectedResult), trim(genDiff($pathToFile1, $pathToFile2, 'plain')));
+        $expectedResult = "Property 'common.follow' was added with value: false\n";
+        $expectedResult .= "Property 'common.setting2' was removed\n";
+        $expectedResult .= "Property 'common.setting3' was updated. From true to null\n";
+        $expectedResult .= "Property 'common.setting4' was added with value: 'blah blah'\n";
+        $expectedResult .= "Property 'common.setting5' was added with value: [complex value]\n";
+        $expectedResult .= "Property 'common.setting6.doge.wow' was updated. From '' to 'so much'\n";
+        $expectedResult .= "Property 'common.setting6.ops' was added with value: 'vops'\n";
+        $expectedResult .= "Property 'group1.baz' was updated. From 'bas' to 'bars'\n";
+        $expectedResult .= "Property 'group1.nest' was updated. From [complex value] to 'str'\n";
+        $expectedResult .= "Property 'group2' was removed\n";
+        $expectedResult .= "Property 'group3' was added with value: [complex value]\n";
+        
+        $this->assertEquals($expectedResult, genDiff($pathToFile1, $pathToFile2, 'plain'));
     }
   }
